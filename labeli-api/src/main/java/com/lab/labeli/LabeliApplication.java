262259b0b4e89837b00f6312b12b4d2b8ac59658
DavidVelaz17 <139006505+DavidVelaz17@users.noreply.github.com>
2024-02-19 18:42:22 -0600

    Spring Initializr

---
+++ labeli-api/src/main/java/com/lab/labeli/LabeliApplication.java
@@ -0,0 +1,13 @@
+package com.lab.labeli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabeliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabeliApplication.class, args);
+	}
+
+}
---
+++ labeli-api/src/test/java/com/lab/labeli/LabeliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lab.labeli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabeliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
