5d407284c911b13c71aefc314b70a8cf236351d2
Chris CR <chris_49git@outlook.com>
2024-02-22 21:18:18 -0600

    I added aplication 'n' security config

---
+++ labeli-api/src/main/java/com/lab/labeli/config/AplicationConfig.java
@@ -0,0 +1,18 @@
+package com.lab.labeli.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+public class AplicationConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedHeaders("*")
+                .allowedMethods("*");
+    }
+}
