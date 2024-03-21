27cb550f38560c885d2c76cf6f2bc0daeaf80d02
DavidVelaz17 <david.lara.velaz@gmail.com>
2024-03-21 15:31:22 -0600

    Order service, entity, dto and form created

---
+++ labeli-api/src/main/java/com/lab/labeli/entity/Order.java
@@ -0,0 +1,57 @@
+package com.lab.labeli.entity;
+
+import com.lab.labeli.form.OrderForm;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Table(name = "orders")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "idorders",nullable = false,unique = true)
+    private int idOrders;
+
+    @Column(name = "idcustomers", nullable = false)
+    private int idCustomers;
+
+    @Column(name = "idusers", nullable = false)
+    private int idUsers;
+
+    @Column(name = "order_timestamp", nullable = false)
+    private LocalDate orderTimeStamp;
+
+    @Column(name = "order_deposit", nullable = false)
+    private int orderDeposit;
+
+    @Column(name = "oorder_total", nullable = false)
+    private int orderTotal;
+
+    @Column(name = "order_notes", nullable = false)
+    private String orderNotes;
+
+    public Order(final OrderForm form){
+        this.idCustomers=form.getIdCustomers();
+        this.idUsers=form.getIdUsers();
+        this.orderTimeStamp=form.getOrderTimeStamp();
+        this.orderDeposit=form.getOrderDeposit();
+        this.orderTotal= form.getOrderTotal();
+        this.orderNotes=form.getOrderNotes();
+    }
+
+    public void updateOrder(final OrderForm form){
+        this.idCustomers=form.getIdCustomers();
+        this.idUsers=form.getIdUsers();
+        this.orderTimeStamp=form.getOrderTimeStamp();
+        this.orderDeposit=form.getOrderDeposit();
+        this.orderTotal= form.getOrderTotal();
+        this.orderNotes=form.getOrderNotes();
+    }
+
+}
