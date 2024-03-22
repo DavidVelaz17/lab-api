65b66bb72088a510d16c74009c58eeb88fc2324e
Chris <104177004+Chris-THC@users.noreply.github.com>
2024-03-21 19:36:16 -0700

    Id-23-tests-BE (#15)
    * I updated Test CRUD 'n' is working
    
    * I updated Test CRUD 'n' is working
    
    * I updated Test CRUD
    
    * I updated CRUD 'n' is working

--- labeli-api/src/main/java/com/lab/labeli/repository/CustomerTestRepository.java
+++ labeli-api/src/main/java/com/lab/labeli/repository/CustomerTestRepository.java
@@ -2,6 +2,8 @@ package com.lab.labeli.repository;
 
 import com.lab.labeli.entity.CustomerTest;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CustomerTestRepository extends JpaRepository<CustomerTest, Integer> {
 }
