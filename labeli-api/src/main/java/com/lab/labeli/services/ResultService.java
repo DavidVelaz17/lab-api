9edc532ecd0644630e3c3f861fc7e854f3d5ca2e
Chris CR <chris_49git@outlook.com>
2024-03-27 20:37:41 -0600

    I added the connection between contents_results and contents tables

--- labeli-api/src/main/java/com/lab/labeli/services/ResultService.java
+++ labeli-api/src/main/java/com/lab/labeli/services/ResultService.java
@@ -1,6 +1,8 @@
 package com.lab.labeli.services;
 
+import com.lab.labeli.dto.ContentsResultsDTO;
 import com.lab.labeli.dto.ResultDTO;
+import com.lab.labeli.entity.ContentsResults;
 import com.lab.labeli.entity.Result;
 import com.lab.labeli.form.ResultForm;
 import com.lab.labeli.repository.ResultRepository;
@@ -10,6 +12,7 @@ import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
@@ -17,19 +20,31 @@ import java.util.List;
 public class ResultService {
 
     private final ResultRepository resultRepository;
+    private final ContentsResultsService contentsResultsService;
 
     @Value("${not.found}")
     private String notFound;
 
+    private Map<Integer, ContentsResultsDTO> getContentsResultsIdsMap(final List<Integer> contentsId) {
+        return contentsResultsService.getIdListByContentResults(contentsId);
+    }
+
     public List<ResultDTO> getAllResult() {
-        final List<Result> getAll = resultRepository.findAll();
-        return getAll.stream().map(ResultDTO::build).toList();
+        final List<Result> resultsList = resultRepository.findAll();
+        final Map<Integer, ContentsResultsDTO> contResultsListId = getContentsResultsIdsMap(resultsList.stream().map(Result::getIdResults).toList());
+        return resultsList
+                .stream()
+                .map(resultAndContentResults -> ResultDTO.build(
+                        resultAndContentResults, contResultsListId.get(resultAndContentResults.getIdResults())
+                ))
+                .toList();
     }
 
     public ResultDTO getResultById(final int idResult) throws Exception {
         validateIfResultExists(idResult);
-        final Result result = resultRepository.findById(idResult).get();
-        return ResultDTO.build(result);
+        final Result resultBodyInfo = resultRepository.findById(idResult).get();
+        final ContentsResultsDTO contentsResultsBodyInfo = contentsResultsService.getContentsResultsById(resultBodyInfo.getIdResults());
+        return ResultDTO.build(resultBodyInfo, contentsResultsBodyInfo);
     }
 
     public void deleteResult(final int idResult) throws Exception {
