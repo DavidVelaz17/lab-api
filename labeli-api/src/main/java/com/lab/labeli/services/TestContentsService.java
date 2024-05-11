dc877e00da1946ce97b3ced5d97d390d3871deb5
Chris CR <chris_49git@outlook.com>
2024-05-10 21:47:07 -0600

    I added the query that 'u' can search TestContents by TestId

--- labeli-api/src/main/java/com/lab/labeli/services/TestContentsService.java
+++ labeli-api/src/main/java/com/lab/labeli/services/TestContentsService.java
@@ -1,7 +1,9 @@
 package com.lab.labeli.services;
 
+import com.lab.labeli.dto.ContentsDTO;
 import com.lab.labeli.dto.TestContentsDTO;
 import com.lab.labeli.dto.TestDTO;
+import com.lab.labeli.entity.Contents;
 import com.lab.labeli.entity.Test;
 import com.lab.labeli.entity.TestContents;
 import com.lab.labeli.form.TestContentsForm;
@@ -11,6 +13,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Value;
+
 import java.util.List;
 import java.util.Map;
 
@@ -19,43 +22,54 @@ import java.util.Map;
 @PropertySource("classpath:ValidationsMessages.properties")
 public class TestContentsService {
     private final TestContentsRepository testContentsRepository;
-    private final TestService testService;
+    private final ContentService contentService;
     private String notFound;
 
-    private Map<Integer, TestDTO> getTestIdsMap(final List<Integer> contentsId) {
-        return testService.getIdListByTest(contentsId);
+
+    private Map<Integer, ContentsDTO> getContentsIdsMap(final List<Integer> contentsId) {
+        return contentService.getIdListByContents(contentsId);
     }
-public List <TestContentsDTO> getAllTestContents(){
-    final List<TestContents> testContentsList= testContentsRepository.findAll();
-    final Map<Integer, TestDTO> contTestContentsListId = getTestIdsMap(testContentsList.stream().map(TestContents::getIdTestContents).toList());
-    return testContentsList.stream().map(resultAndContentResults -> TestContentsDTO.build(resultAndContentResults, contTestContentsListId.get(resultAndContentResults.getIdTestContents()))).toList();
-}
-public TestContentsDTO getTestContentsById(final int idTest) throws Exception {
-    validateIfTestContentsExists(idTest);
-    final TestContents testContents = testContentsRepository.findById(idTest).get();
-    return TestContentsDTO.build(testContents);
-}
 
-public void deleteTestContents(final int idTest) throws Exception {
-    validateIfTestContentsExists(idTest);
-    testContentsRepository.deleteById(idTest);
-}
-public TestContentsDTO createTestContents(final TestContentsForm form) {
-    final TestContents testContent = new TestContents(form);
-    testContentsRepository.save(testContent);
-    return TestContentsDTO.build(testContent);
-}
+    public List <TestContentsDTO> getAllTestContents(){
+        final List<TestContents> getAllContentList= testContentsRepository.findAll();
+        final Map<Integer, ContentsDTO> contTestContentsListId = getContentsIdsMap(getAllContentList.stream().map(TestContents::getContentId).toList());
+        return getAllContentList.stream().map(resultAndContentResults -> TestContentsDTO.build(resultAndContentResults, contTestContentsListId.get(resultAndContentResults.getIdTestContents()))).toList();
+    }
 
-public TestContentsDTO updateCustomer(final TestContentsForm form, final int idTest) throws Exception {
-    validateIfTestContentsExists(idTest);
-    final TestContents testContents = testContentsRepository.findById(idTest).get();
-    testContents.updateTestContentsFunction(form);
-    testContentsRepository.save(testContents);
-    return TestContentsDTO.build(testContents);
-}
+    public List<TestContentsDTO> getTestContentsByIdTest(final int idTest) throws Exception{
+        final List<TestContents> getAllIdContentsId = testContentsRepository.findAllByTestsId(idTest);
+        final Map<Integer, ContentsDTO> contentsListId = getContentsIdsMap(getAllIdContentsId.stream().map(TestContents::getContentId).toList());
+        return getAllIdContentsId.stream().map(contentsBody -> TestContentsDTO.build(contentsBody,contentsListId.get(contentsBody.getContentId()))).toList();
+    }
 
-private void validateIfTestContentsExists(final int idTest) throws Exception{
-    if(!testContentsRepository.existsById(idTest)){
+
+    public TestContentsDTO getTestContentsById(final int idTest) throws Exception {
+        validateIfTestContentsExists(idTest);
+        final TestContents testContents = testContentsRepository.findById(idTest).get();
+        return TestContentsDTO.build(testContents);
+    }
+
+    public void deleteTestContents(final int idTest) throws Exception {
+        validateIfTestContentsExists(idTest);
+        testContentsRepository.deleteById(idTest);
+    }
+
+    public TestContentsDTO createTestContents(final TestContentsForm form) {
+        final TestContents testContent = new TestContents(form);
+        testContentsRepository.save(testContent);
+        return TestContentsDTO.build(testContent);
+    }
+
+    public TestContentsDTO updateCustomer(final TestContentsForm form, final int idTest) throws Exception {
+        validateIfTestContentsExists(idTest);
+        final TestContents testContents = testContentsRepository.findById(idTest).get();
+        testContents.updateTestContentsFunction(form);
+        testContentsRepository.save(testContents);
+        return TestContentsDTO.build(testContents);
+    }
+
+    private void validateIfTestContentsExists(final int idTest) throws Exception {
+        if (!testContentsRepository.existsById(idTest)) {
             throw new Exception(notFound);
         }
     }
