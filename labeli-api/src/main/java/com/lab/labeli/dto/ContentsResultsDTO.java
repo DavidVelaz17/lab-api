154fa9472d7e5c12474a3a6d304900e036081847
Chris CR <chris_49git@outlook.com>
2024-05-21 19:51:03 -0600

    I fix some stuff into the ContentsResults :(

--- labeli-api/src/main/java/com/lab/labeli/dto/ContentsResultsDTO.java
+++ labeli-api/src/main/java/com/lab/labeli/dto/ContentsResultsDTO.java
@@ -1,4 +1,5 @@
 package com.lab.labeli.dto;
+
 import com.lab.labeli.entity.ContentsResults;
 import jakarta.validation.constraints.Size;
 import lombok.Builder;
@@ -8,24 +9,37 @@ import org.jsondoc.core.annotation.ApiObjectField;
 @Getter
 @Builder
 public class ContentsResultsDTO {
-    @ApiObjectField(name= "contentResultId", description = "Content Result Id")
+    @ApiObjectField(name = "contentResultId", description = "Content Result Id")
     private int contResultId;
 
-    @ApiObjectField(name= "resultId", description = "Result Id")
+    @ApiObjectField(name = "resultId", description = "Result Id")
     private int resultId;
 
-    @ApiObjectField(name= "contentId", description = "Content Ids")
+    @ApiObjectField(name = "contentId", description = "Content Ids")
     private int contentId;
 
-    @ApiObjectField(name= "resultValue", description = "Result value")
-    @Size(max = 100,message = "{right.length}")
+    @ApiObjectField(name = "resultValue", description = "Result value")
+    @Size(max = 100, message = "{right.length}")
     private String resultValue;
 
-    public static ContentsResultsDTO build(final ContentsResults content){
+    @ApiObjectField(name = "contentsDTO", description = "This is the contentsDTO body")
+    private ContentsDTO contentsDTO;
+
+    public static ContentsResultsDTO build(final ContentsResults content) {
         return ContentsResultsDTO.builder()
                 .contResultId(content.getContentResultId())
                 .resultId(content.getResultId())
                 .contentId(content.getContentId())
+                .resultValue(content.getResultValue())
+                .build();
+    }
+
+    public static ContentsResultsDTO build(final ContentsResults content, final ContentsDTO contentsDTOInfo) {
+        return ContentsResultsDTO.builder()
+                .contResultId(content.getContentResultId())
+                .resultId(content.getResultId())
+                .contentId(content.getContentId())
+                .contentsDTO(contentsDTOInfo)
                 .resultValue(content.getResultValue())
                 .build();
     }
