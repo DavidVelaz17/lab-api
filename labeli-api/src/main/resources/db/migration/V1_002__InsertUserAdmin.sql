56f392d37feae3a52952b5e883233c35ac1faccc
DavidVelaz17 <david.lara.velaz@gmail.com>
2024-08-15 20:14:42 -0600

    user admin updated for testing purposes

--- labeli-api/src/main/resources/db/migration/V1_002__InsertUserAdmin.sql
+++ labeli-api/src/main/resources/db/migration/V1_002__InsertUserAdmin.sql
@@ -1,1 +1,1 @@
-INSERT INTO users VALUES ('1','David Gustavo Lara Velazquez','34','2721267402','Zimpizahua 17D','$2a$10$xu5Y1rV/ovJH2hQuNGeJou3Hs3o1uRxYeUudfJwSu/ztzLQm2muiG','0');
+INSERT INTO users VALUES ('1','Admin','34','2791267302','Avenida Hidalgo 17','$2a$10$xu5Y1rV/ovJH2hQuNGeJou3Hs3o1uRxYeUudfJwSu/ztzLQm2muiG','0');
